47cd098642249d40a1b5388b9b5b3ed8739d9a23
tejaswar <rajt18165@gmail.com>
2023-02-24 08:53:49 +0530

    my commit in calculator

---
+++ javascript.js
@@ -0,0 +1,37 @@
+let buttons=document.getElementsByTagName('button');
+let outputs=document.getElementById('output');
+
+
+for(i=0;i<buttons.length;i++){
+    buttons[i].onclick=function(){
+        let input=this.innerText;
+        display(input)
+    }
+}
+function display(input){
+    let current=outputs.innerText
+    if (current=='0'){
+        if(input!='c'&&input!='back'&&input!='='){
+            outputs.innerText=''
+            outputs.innerText+=input
+        }
+
+    }else{
+        if (input=='back'){
+            output.innerText=current.substring(0,current.length-1)
+            if (outputs.innerText==''){
+                outputs.innerText='0'
+            }
+        }
+        if(input=='c'){
+            outputs.innerText='0'
+        }
+        if (input!='c'&&input!='back'&&input!='='){
+            outputs.innerText+= input
+        }
+        if (input=='='){
+            let show=outputs.innerText
+            outputs.innerText= eval(show)
+        }
+    }
+}
